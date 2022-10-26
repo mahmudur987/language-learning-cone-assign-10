dff82fcfed6a406911f7660e51168fce5d456c39
mahmud987 <safemahmud987@gmail.com>
2022-10-26 08:50:30 +0600

    cheakout doned

--- src/Components/CheakOut/Cheakout.js
+++ src/Components/CheakOut/Cheakout.js
@@ -9,7 +9,7 @@ const Cheakout = () => {
     return (
         <div className='p-4 border border-blue-600 m-5 rounded-lg'>
 
-            <h1 className='text-3xl font-bold m-5 '> Course Name :{cart.name}</h1>
+            <h1 className='text-3xl font-bold m-5 text-rose-700 '> Course Name :{cart.name}</h1>
             <h1 className='text-2xl font-bold m-5 '> Course title :{cart.title}</h1>
             <h1 className='text-2xl font-bold m-5 '> Group Name :{cart.company}</h1>
             <h1 className='text-2xl font-bold m-5 '> Phone number :{cart.phone}</h1>
