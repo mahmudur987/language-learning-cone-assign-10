3cd661a4eb8613ff31a3492648b6d26416a5ab1b
mahmudur987 <safemahmud987@gmail.com>
2022-12-21 21:15:50 +0600

    responsive done"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
-import { RouterProvider } from 'react-router-dom';
-import './App.css';
-import route from './Routes/Route/Route';
-import { Toaster } from 'react-hot-toast';
-import { useContext } from 'react';
-import { authContext } from './UserContext/UserContext';
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import route from "./Routes/Route/Route";
+import { Toaster } from "react-hot-toast";
+import { useContext } from "react";
+import { authContext } from "./UserContext/UserContext";
 function App() {
   const { theme } = useContext(authContext);
   return (
-    <div className={` ${theme ? "bg-slate-300" : " bg-slate-100"}`} data-theme="cupcake"  >
-
+    <div
+      className={` ${theme ? "bg-slate-400" : " bg-slate-100"}`}
+      data-theme="cupcake"
+    >
       <RouterProvider router={route}></RouterProvider>
 
       <Toaster></Toaster>
